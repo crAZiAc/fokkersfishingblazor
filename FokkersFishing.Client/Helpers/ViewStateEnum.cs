0583b0ddfac2bcb3991841cbcf3e10ef60a8e885
Tom de Ridder <tom.de.ridder@valueblue.com>
2023-09-24 20:39:35 +0200

    Unified catches list visual

---
+++ FokkersFishing.Client/Helpers/ViewStateEnum.cs
@@ -0,0 +1,11 @@
+﻿namespace FokkersFishing.Client.Helpers
+{
+    public enum ViewStateEnum
+    {
+        Admin,
+        Personal,
+        Team,
+        Pending,
+        CompetitionLeaderboard
+    } // end enum
+} // end c
